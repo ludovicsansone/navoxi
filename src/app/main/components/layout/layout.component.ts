428885ca890857f1ea310478d581922b9ce5db91
ludovicsansone <ludovicsansone@Mac-mini-de-ludovic.local>
2018-11-07 07:37:20 +0100

    Changing files to SNCF live demo

--- src/app/main/components/layout/layout.component.ts
+++ src/app/main/components/layout/layout.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'main-layout',
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LayoutComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+  private router: Router) { }
 
   ngOnInit() {
+      this.router.navigate(['sncf-home']);
   }
 
 }
