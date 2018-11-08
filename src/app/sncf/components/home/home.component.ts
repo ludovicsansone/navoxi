31cdee8e7b0e5340af94bb7e5f6dd2d69aab30a4
ludovicsansone <ludovicsansone@Mac-mini-de-ludovic.local>
2018-11-08 10:29:44 +0100

    Changing html files: layout and home; and ts file : home

--- src/app/sncf/components/home/home.component.ts
+++ src/app/sncf/components/home/home.component.ts
@@ -17,9 +17,9 @@ export class HomeComponent implements OnInit {
 
   ngOnInit() {
       this.panel = [
-      {destination: 'Valence Ville', numero: '886712', heure: '10h42', quai: 'A'},
-      {destination: 'Clermont-Ferrand', numero: '874442', heure: '10h54', quai: 'B'},
-      {destination: 'Paris Gare de Lyon', numero: '5146', heure: '11h08', quai: 'C'},
+      {typeTrain: 'TER', destination: 'Valence Ville', numero: '886712', heure: '10h42', quai: 'A'},
+      {typeTrain: 'TER', destination: 'Clermont-Ferrand', numero: '874442', heure: '10h54', quai: 'B'},
+      {typeTrain: 'TGV', destination: 'Paris Gare de Lyon', numero: '5146', heure: '11h08', quai: 'C'},
       ];
   }
 
